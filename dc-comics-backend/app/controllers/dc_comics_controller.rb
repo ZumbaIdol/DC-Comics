ec78e71bb35ada57ac17900bc7bdc6630a191672
ZumbaIdol <david.a.foote@gmail.com>
2020-06-08 13:44:24 -0500

    Added index actions for users and comics

--- dc-comics-backend/app/controllers/dc_comics_controller.rb
+++ dc-comics-backend/app/controllers/dc_comics_controller.rb
@@ -1,2 +1,6 @@
 class DcComicsController < ApplicationController
+    def index
+        dc_comics = DcComic.all
+        render json: dc_comics, include: [:user]
+    end
 end
--- dc-comics-backend/app/controllers/users_controller.rb
+++ dc-comics-backend/app/controllers/users_controller.rb
@@ -1,2 +1,6 @@
 class UsersController < ApplicationController
+    def index
+        users = User.all
+        render json: users, include: [:dc_comics]
+    end
 end
