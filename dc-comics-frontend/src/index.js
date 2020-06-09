b35ca174a8144a4202692fe68904c720bf34595c
ZumbaIdol <david.a.foote@gmail.com>
2020-06-08 19:01:41 -0500

    Added a little styling, a lot of work left to do on that after getting functionality together

--- dc-comics-frontend/src/index.js
+++ dc-comics-frontend/src/index.js
@@ -3,22 +3,19 @@ const USERS_URL = `${BASE_URL}/users`
 const COMICS_URL = `${BASE_URL}/dc_comics`
 const main = document.querySelector('main')
 
-// document.addEventListener('DOMContentLoaded', function() {
-//     fetch()
-// })
+document.addEventListener('DOMContentLoaded', function() {
+    fetchUsers()
+})
 
-fetch(`${BASE_URL}/test`)
-  .then(response => response.json())
-  .then(parsedResponse => console.log(parsedResponse))
+function fetchUsers() {
+    fetch(USERS_URL)
+    .then(function(resp) {
+        return resp.json()
+    })
+    .then(function(users) {
+        for (const user of users) {
+            makeUserCard(user)
+        }
+    })
+}
 
-// function fetchUsers() {
-//     fetch(USERS_URL)
-//     .then(function(resp) {
-//         return resp.json()
-//     })
-//     .then(function(users) {
-//         for (const user of users) {
-//             makeUserCard(user)
-//         }
-//     })
-// }
