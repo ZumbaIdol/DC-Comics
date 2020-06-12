77895ee5b52cfaf9158c770d568ef41105a64262
ZumbaIdol <david.a.foote@gmail.com>
2020-06-12 11:29:55 -0500

    Added user form for adding users and create user action in user controller

--- dc-comics-backend/app/controllers/users_controller.rb
+++ dc-comics-backend/app/controllers/users_controller.rb
@@ -8,4 +8,18 @@ class UsersController < ApplicationController
         user = User.find_by(id: params[:id])
         render json: user, include: [:dc_comics]
     end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+          session[:user_id] = @user.id
+          render json:  user, include: [:dc_comics]
+        end
+    end
+    
+      private
+    
+      def user_params
+        params.require(:user).permit(:name)
+      end
 end
